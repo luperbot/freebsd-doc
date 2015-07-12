aef749de4f89c3e1ca58e2258a87f194c9210b00
Claudia Mane <c@luperlabs.com>
2015-07-12 22:39:14 +0000

    First build all the .xml files into .html files, then convert to .rst from there.

--- tools/docbook_to_rst.py
+++ tools/docbook_to_rst.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Script is to enable converting .xml docs from DocBook format into reStructuredText format.
+"""Script is to enable converting .html docs from DocBook format into reStructuredText format.
 This script should be run on all the old version of docs as to enable readthedocs building
 each version.
 """
@@ -8,13 +8,9 @@ import os
 
 from subprocess import call
 
-def xml_to_rst(file, root, single=False, encoding='UTF-8', html=False):
+def html_to_rst(file, root, single=False, encoding='UTF-8', html=True):
     original_format= 'html' if html else 'docbook'
-    if not file.endswith('.xml'):
-        return
-    # Ignore these files, we don't need them for docs, since they are error
-    # pages.
-    if file in ('4xx.xml', '5xx.xml'):
+    if not file.endswith('.html'):
         return
     new_file = file[:-4] + '.rst'
     file_path = "%s/%s" % (root, file)
@@ -29,9 +25,8 @@ def xml_to_rst(file, root, single=False, encoding='UTF-8', html=False):
         command = "iconv -f %s %s | pandoc -f %s -t rst -s -o %s | iconv -f utf-8" % (
             encoding, file_path, original_format, new_file_path)
         call(command, shell=True)
-    append_title(file_path, new_file_path)
     #TODO: Only delete file if the new file is not empty.
-    #delete_xml(file_path)
+    #delete_html(file_path)
 
 def add_file_name(file_name):
     if all_file_names.get(file_name):
@@ -39,19 +34,14 @@ def add_file_name(file_name):
     else:
         all_file_names[file_name] = 1
 
-def append_title(file_path, new_file_path):
-    """Pandoc does not convert data within title tags automatically.
-    This needs to be parsed and appended to the top of the new file."""
-    pass
-
-def delete_xml(file_path):
-    """Delete .xml file once converted to .rst as part of cleanup."""
+def delete_html(file_path):
+    """Delete .html file once converted to .rst as part of cleanup."""
     call(['rm', file_path])
 
 def is_single(files):
     count = 0
     for file in files:
-        if file.endswith('.xml'):
+        if file.endswith('.html'):
             count += 1
     if count == 1:
         return True
@@ -59,14 +49,19 @@ def is_single(files):
 
 def convert_dir(dir_path):
     """dir_path is the path name of all the files to convert (ex en_US.ISO8859-1)"""
-    print "Converting .xml files to .rst for %s" % dir_path
+    print "Converting .html files to .rst for %s" % dir_path
     encoding = dir_path.split('/')[1].split('.')[1]
     print "Original encoding is: %s" % encoding
     for root, dirs, files in os.walk(dir_path):
         single = is_single(files)
-        html = 'htdocs' in root
         for file in files:
-            xml_to_rst(file, root, single=single, encoding=encoding, html=html)
+            html_to_rst(file, root, single=single, encoding=encoding, html=True)
+
+def create_htdocs(dir_path):
+    htdocs_path = "%s%s" % (dir_path, 'htdocs/')
+    print "Making htdocs."
+    call(['cd', htdocs_path, '&&', 'make all'])
+    print "Done making htdocs."
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -74,4 +69,5 @@ if __name__ == '__main__':
         print "\tex. python tools/docbook_to_rst.py docs/en_US.ISO8859-1/"
     else:
         dir_path = sys.argv[1]
+        create_htdocs(dir_path)
         convert_dir(dir_path)
